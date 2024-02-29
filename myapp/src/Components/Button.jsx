613aa196ad7b987ef8fd8de873ee30f8e06143b4
Toshan <toshanyt@gmail.com>
2024-02-29 22:52:46 +0530

    stylized button

--- myapp/src/Components/Button.jsx
+++ myapp/src/Components/Button.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React ,{useRef} from "react";
 import './button.css';
+import {motion, transform, useScroll} from 'framer-motion';
 
 export default function Button(props){
+    const scrollRef = useRef(null);
     return(
         <>
-            <a href = {props.link} className="button">
+            <motion.a initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ root: scrollRef }} href = {props.link}  className="button">
                 <button className="button">{props.title}</button>
-            </a>
+            </motion.a>
         </>
     )
 }
